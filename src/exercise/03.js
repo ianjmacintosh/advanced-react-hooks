bb2014a89ccea00ce7c10ae3c0277b1d362acf35
Ian MacIntosh <ian@ianjmacintosh.com>
2024-08-08 19:57:13 +0000

    Finish Ex3.01

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -19,16 +19,24 @@ function CountProvider({children}) {
   return <CountContext.Provider value={value}>{children}</CountContext.Provider>
 }
 
+function useCount() {
+  const value = React.useContext(CountContext)
+  if (value) {
+    return value
+  } else {
+    throw new Error('`useCount` can only be used inside a <CountProvider>')
+  }
+}
+
 function CountDisplay() {
   // 🐨 get the count from useContext with the CountContext
-  const count = React.useContext(CountContext)[0]
-  console.log(React.useContext(CountContext))
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
   // 🐨 get the setCount from useContext with the CountContext
-  const setCount = React.useContext(CountContext)[1]
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -36,14 +44,8 @@ function Counter() {
 function App() {
   return (
     <div>
-      {/*
-        🐨 wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
-      <CountProvider>
-        <CountDisplay />
-        <Counter />
-      </CountProvider>
+      <CountDisplay />
+      <Counter />
     </div>
   )
 }
